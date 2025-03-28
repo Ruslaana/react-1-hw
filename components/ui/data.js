b84582acaa3469a4adc7882360c6167ebab95f30
Ruslana Onyshchuk <lalelya7@gmail.com>
2025-03-28 15:19:56 +0100

    done with main part with homework

---
+++ components/ui/data.js
@@ -0,0 +1,34 @@
+export const pages = [
+  { href: '/', title: 'Home' },
+  { href: '/about', title: 'About' },
+  { href: '/services', title: 'Services' },
+  { href: '/contact', title: 'Contact' },
+];
+
+export const socialMediaLinks = [
+  {
+    url: 'https://facebook.com',
+    title: 'Facebook',
+    icon: '/socialmedia/facebook.png',
+  },
+  {
+    url: 'https://instagram.com',
+    title: 'Instagram',
+    icon: '/socialmedia/instagram.png',
+  },
+  {
+    url: 'https://tiktok.com',
+    title: 'Tiktok',
+    icon: '/socialmedia/tiktok.png',
+  },
+  {
+    url: 'https://linkedin.com',
+    title: 'LinkedIn',
+    icon: '/socialmedia/linkedin.png',
+  },
+  {
+    url: 'https://google.com',
+    title: 'On the streets at night',
+    icon: '/socialmedia/google.png',
+  },
+];
